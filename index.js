9e86722c81e914c555f7685aba8910061e271acf
Stefan Tanase <111762429+stefan5115@users.noreply.github.com>
2023-08-24 16:41:47 +0300

    am terminat proiectul

--- index.js
+++ index.js
@@ -1,164 +1,111 @@
-let currentPlayer = 'X'
 
-btnRestart.addEventListener('click', restart)
+// Game Module containing all code except for two event listeners at bottom
+// All variables and functions private except for the 2 called by event listeners
+let gameModule = (function() {
+    'use strict';
+    // all module code here
+    // variables
+    const gameStatusMessage = document.getElementById('game-status');
+    let gameActive = true;
+    let currentPlayer = "X";
+    let gameBoard = ["","","","","","","","",""]; 
+    
+    const winningMessage = () => `${currentPlayer} wins!`
+    const tieMessage = "It's a tie";
+    const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+    gameStatusMessage.innerText = currentPlayerTurn();
 
-const Player = (sign) =>{
-    const getSign = () => sign
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
 
-    return {getSign}
-}
-
-
-const gameBoard = (() =>{
-    const game = ['','','','','','','','','']
-    const getSquare = (index) => {
-        return game[index];
-    };
-
-    const setSquare = (index, sign) => {
-        if (index >= 0 && index < game.length) {
-            game[index] = sign;
+    // main public function for gameplay
+    function handleClick(e) {
+        const clickedSquare = e.target;
+        const clickedSquareNumber = parseInt(clickedSquare.getAttribute('data-key'));
+      
+        if (gameBoard[clickedSquareNumber] !== "" || !gameActive) {
+            return;
         }
-    };
-    const reset = () => {
-        for (let i = 0; i < game.length; i++) {
-            game[i] = '';
+      
+        handlePlay(clickedSquare, clickedSquareNumber);
+        handleResult();
+    }
+    // private function if player chooses valid square
+    function handlePlay(clickedElement, squareIndex) {
+        gameBoard[squareIndex] = currentPlayer;
+        clickedElement.innerText = currentPlayer;
+        if (currentPlayer === "X") {
+          clickedElement.style.color = '#219EBC';
+        } else {
+          clickedElement.style.color = '#FB8500';
         }
-    };
+    }
+    // private function to check if game is finished and transition player turns
+    function handleResult() {
+        let gameWon = false;
+        for (let i = 0; i <= 7; i++) {
+          let a = gameBoard[winningCombos[i][0]];
+          let b = gameBoard[winningCombos[i][1]];
+          let c = gameBoard[winningCombos[i][2]];
+          
+          if (!a || !b || !c) {
+            continue;
+          }
+          if (a === b && b === c) {
+            gameWon = true;
+            break;
+            }
+        }
+        if (gameWon) {
+            gameStatusMessage.innerText = winningMessage();
+            changeMessageColor('004466')
+            gameActive = false;
+            return;
+        }
+        let tieGame = !gameBoard.includes("");
+        if (tieGame) {
+            gameStatusMessage.innerText = tieMessage;
+            changeMessageColor('004466')
+            gameActive = false;
+            return;
+        }
+        changePlayer();
+    }
+    // update message color to correspond with upcoming player turn
+    function changeMessageColor(color) {
+        gameStatusMessage.style.color = "#" + color;
+    }
+    // change to next player's turn after move
+    function changePlayer() {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        gameStatusMessage.innerText = currentPlayerTurn();
+        if (currentPlayer === "X") {
+            changeMessageColor('219EBC')
+        } else {
+            changeMessageColor('FB8500')
+        }
+      }
+    
+    function restartGame() {
+        gameBoard = ["","","","","","","","",""];
+        document.querySelectorAll('.board-square').forEach(square => square.innerText = "");
+        gameActive = true;
+        currentPlayer = "X";
+        gameStatusMessage.innerText = currentPlayerTurn();
+      }
 
-    return {getSquare, setSquare, reset}
+    // list public functions and variables  
+    return { handleClick, restartGame };
 })();
 
-
-const displayController = (() =>{
-    const squareElements = document.querySelectorAll('.square')
-    const btnRestart = document.getElementById('restart')
-
-    squareElements.forEach((square, index) => {
-        square.addEventListener('click', (e) => {
-            if (gameController.getIsOver() || e.target.textContent !== '') {
-                return;
-            }
-            gameController.playRound(index);
-            updateGameboard();
-        });
-    });
-
-    const updateGameboard = () => {
-        for (let i = 0; i < squareElements.length; i++) {
-            squareElements[i].textContent = gameBoard.getSquare(i);
-        }
-    };
-    
-    const setResultMessage = (winner) => {
-        if (winner === "Draw") {
-          return console.log("It's a draw!");
-        } else {
-          return console.log(`Player ${winner} has won!`);
-        }
-      };
-    
-      return {setResultMessage}
-
-})()
-
-const gameController = (() => {
-    const playerX = Player('X')
-    const player0 = Player('0')
-    let round = 1
-    let isOver = false
-
-    const playRound = (squareIndex) => {
-        gameBoard.setSquare(squareIndex ,getCurrentPlayerSign())
-        if(checkWinner(squareIndex)){
-            isOver = true
-            return console.log('win')
-        }
-        if (round === 9){
-            isOver = true 
-            return console.log('draw')
-        }
-        round ++
-    }
-
-    const getCurrentPlayerSign = () => {
-        return round % 2 === 1 ? playerX.getSign() : player0.getSign()
-    }
-
-    const checkWinner = (fieldIndex) => {
-        const winConditions = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-    
-        return winConditions
-          .filter((combination) => combination.includes(squareIndex))
-          .some((possibleCombination) =>
-            possibleCombination.every(
-              (index) => gameBoard.getField(index) === getCurrentPlayerSign()
-            )
-          );
-      };
-
-      const getIsOver = () => {
-        return isOver
-      }
-
-      const reset = () => {
-        round = 1;
-        isOver = false 
-      }
-
-      return {playRound, getIsOver, reset }
-    })()
-
-// cells.forEach((cell, index) => {
-//     cell.addEventListener('click',()=> makeMove(index))
-// });
-
-
-// function restart(){
-//     cells.forEach(cell => {
-//             cell.innerHTML = '' 
-//     });
-
-//     game = ['','','','','','','','','']
-// }
-
-
-// function makeMove(index){
-//     if(game[index] === ''){
-//         game[index] = currentPlayer;
-//         cells[index].innerHTML = currentPlayer
-//         currentPlayer = currentPlayer === 'X' ? '0' : 'X'
-//     }
-// }
-
-// const winConditions = [[0,1,2],[2,5,8],[0,3,6],[6,7,8],[0,4,8],[2,4,6],[1,4,7],[3,4,5]]
-
-// function checkGameOver(){
-//     if(game[0] == game[1] == game[2] || 
-//         game[2] == game[5] == game[8] ||
-//         game[0] == game[3] == game[6] ||
-//         game[6] == game[7] == game[8] ||
-//         game[0] == game[4] == game[8] ||
-//         game[2] == game[4] == game[6] ||
-//         game[1] == game[4] == game[7] ||
-//         game[3] == game[4] == game[6] ){
-//             return 'win'
-//         }else if(!game.includes('')){
-//             return 'tie'
-//         }else{
-//             return 'play'
-//         }
-// }
-
-// let gameOver = false;
-
-
+//
+document.querySelectorAll('.board-square').forEach(square => square.addEventListener('click', gameModule.handleClick));
+document.getElementById('restart-game').addEventListener('click', gameModule.restartGame);
